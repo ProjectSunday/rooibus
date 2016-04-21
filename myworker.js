fca887fecbde01ccf21e71aa0750f9059ca0d5d7
Hai Nguyen <hai.nguyen@salesforce.com>
2016-04-21 19:05:54 -0400

    forgot myworker.js

---
+++ myworker.js
@@ -0,0 +1,22 @@
+
+function getLocation() {
+
+	// navigator.geolocation.getCurrentPosition(function (loc) { 
+
+		var now = new Date()
+
+		var msg = now.toString()
+
+		// msg += ' accuracy: ' + loc.coords.accuracy
+		// msg += ' longitude: ' + loc.coords.longitude
+		// msg += ' latitude: ' + loc.coords.latitude
+
+		postMessage(msg)
+
+		setTimeout(getLocation, 1000)
+
+	// })
+
+}
+
+getLocation()
