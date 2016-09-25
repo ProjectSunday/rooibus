2c7866a57e4b7c9645ebcc39c1382b86a1ea6f59
Hai Nguyen <hai.nguyen@hnguyen-ltm1.internal.salesforce.com>
2016-09-25 13:36:44 -0400

    starting work on pwa

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,41 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var node_modules  = path.resolve(__dirname, 'node_modules');
+var node_modules    = path.resolve(__dirname, 'node_modules');
+var src             = path.resolve(__dirname, 'src');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:7000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body'
-    })
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src'),
-      exclude: node_modules,
-    }]
-  }
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:7000',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel'],
+                include: src,
+                exclude: node_modules,
+            },
+            {
+                test: /manifest.json$/,
+                loader: 'file-loader',
+                include: src
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ]
 };
