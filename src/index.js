6cb0c85f322e06aab791a4df2dc4ccbc1b798053
Hai Nguyen <hai.nguyen@hnguyen-ltm1.internal.salesforce.com>
2016-10-23 13:59:35 -0400

    Sun Oct 23 13:59:34 2016

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-//import todoApp from './store/Reducers'
-import App from './App'
+import React 		from 'react'
+import { render } 	from 'react-dom'
+// import { Router } 	from 'react-router'
+// import { Provider }	from 'react-redux'
 
-//let store = createStore(todoApp)
+// import { store, history } 	from '~/store/store'
+// import routes 				from './routes'
+// import { Category, RequestedClass, UpcomingClass }	from './actions/actions'
 
+import Map from './components/map/map'
+
+///////////////////////////////////////////////////////////////////
+// Others
+///////////////////////////////////////////////////////////////////
+// import './index.html'
+// import './utils'
+// import './static/images/favicon.ico'
+// import './index.sass'
+// import '~/static/bootswatch/flatly.min.css'
+
+// import * as images from './static/images'
+// import preloadImages from './static/images/preloadimages'
+
+///////////////////////////////////////////////////////////////////
+// Globals
+///////////////////////////////////////////////////////////////////
+// window.IMAGES = images
+// preloadImages()
+
+///////////////////////////////////////////////////////////////////
+// Initialization
+///////////////////////////////////////////////////////////////////
+// Category.getList()
+// RequestedClass.getList()
+// UpcomingClass.getList()
+
+///////////////////////////////////////////////////////////////////
+// Mount
+///////////////////////////////////////////////////////////////////
 render(
-    <App />,
-  document.getElementById('root')
+	<Map />,
+	document.getElementById('mount')
 )
+
