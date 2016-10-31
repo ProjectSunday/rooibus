bfba69b0b854df18f5528ac3c899b1aca21436de
Hai Nguyen <hai.nguyen@hnguyen-ltm1.internal.salesforce.com>
2016-10-31 11:01:02 -0400

    Mon Oct 31 11:01:02 2016

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -4,25 +4,61 @@ export default class Map extends React.Component {
 	componentDidMount() {
 		// console.log('componentDidMount');
 
-		// attachGoogleMap(this.refs.map)
-		doThings();
+		initGoogleMap(this.refs.map)
+		// doThings();
 	}
 	render() {
 		return (
-			<div ref="map"> yooo 5555 </div>
+			<div ref="map" style={styles.map}> yooo 5555 </div>
 		)		
 	}
 }
 
-async function attachGoogleMap(node) {
-	console.log('huh')
-	await getGoogleObject()
-	return 'yo'
+const styles = {
+	map: {
+		width: '300px',
+		height: '100px'
+	}
+}
+
+async function initGoogleMap(node) {
+	if (!window.initMap) {
+		window.initMap = initMap
+	}
+
+	if (!window.google) {
+		await getGoogleObject()
+	}
+
+
+	var map = new google.maps.Map(node, {
+	    center: {lat: -34.397, lng: 150.644},
+	    scrollwheel: false,
+	    zoom: 8
+	})
+
+
 }
 
 async function getGoogleObject() {
-	console.log('waht')
-	return 'blah'
+	return new Promise((resolve, reject) => {
+		window._rooibusGoogleMapLoaded = function () {
+			console.log('resolving _rooibusGoogleMapLoaded')
+			resolve()
+		}
+
+		var script = document.createElement('script')
+		script.type = 'text/javascript'
+		script.async = false;
+		script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCzx7FndCtugg9jcduxA6pK_IKlr7EJYhY&callback=_rooibusGoogleMapLoaded`
+
+		script.addEventListener('error', function () {
+			console.error('Fatal Error: unable to load the google maps api')
+			reject()
+		})
+
+		document.body.append(script)
+	})
 }
 
 
